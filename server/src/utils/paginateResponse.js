37bf80ddf622e04bce6eb4b56f0fbce631d4a746
Umar Khalilov <ERMASTER100@gmail.com>
2022-03-23 19:55:42 +0200

    Fixed status code

--- server/src/utils/paginateResponse.js
+++ server/src/utils/paginateResponse.js
@@ -4,7 +4,7 @@ module.exports.paginateResponse = ([total, result], page, limit) => {
     const nextPage = page + 1 > lastPage ? null : page + 1;
     const prevPage = page - 1 < 1 ? null : page - 1;
     return {
-        statusCode: 'success',
+        statusCode: 200,
         data: [...result],
         count: total,
         currentPage: page,
