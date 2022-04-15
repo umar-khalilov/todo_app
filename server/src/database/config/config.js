eecb1cfbc5e4a07a3441c6ea380bceb66731232a
Umar Khalilov <ERMASTER100@gmail.com>
2022-04-16 00:04:34 +0300

    Add new configs and refactor code

--- server/src/database/config/config.js
+++ server/src/database/config/config.js
@@ -1,25 +1,26 @@
-'use strict';
-require('dotenv').config();
-const {
-    env: {
-        POSTGRES_USER,
-        POSTGRES_PASSWORD,
-        POSTGRES_DB,
-        POSTGRES_HOSTNAME,
-        DB_DIALECT,
-    },
-} = process;
-
-module.exports = {
-    development: {
-        username: POSTGRES_USER,
-        password: POSTGRES_PASSWORD,
-        database: POSTGRES_DB,
-        host: POSTGRES_HOSTNAME,
-        dialect: DB_DIALECT,
-        migrationStorage: 'json',
-        seederStorage: 'json',
-    },
-    test: {},
-    production: {},
-};
+{
+    "development": {
+    "username": "postgres",
+      "password": "password",
+      "database": "todo-dev",
+      "host": "db-dev",
+      "dialect": "postgres",
+      "operatorsAliases": "Op"
+},
+    "test": {
+    "username": "postgres",
+      "password": "password",
+      "database": "todo-test",
+      "host": "db-test",
+      "dialect": "postgres",
+      "operatorsAliases": "Op"
+},
+    "production": {
+    "username": "postgres",
+      "password": "password",
+      "database": "todo-prod",
+      "host": "db-prod",
+      "dialect": "postgres",
+      "operatorsAliases": "Op"
+}
+}
--- server/upMigrate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npx sequelize db:create
-npx sequelize db:migrate
-npx sequelize db:seed:all
