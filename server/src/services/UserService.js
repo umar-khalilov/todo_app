9ae52f8d61f44ddbd9a6ee8cada24507b9267305
Umar Khalilov <ERMASTER100@gmail.com>
2022-04-22 12:58:20 +0300

    Add new docker configs

--- server/src/services/UserService.js
+++ server/src/services/UserService.js
@@ -18,6 +18,8 @@ module.exports = class UserService {
             ],
         });
 
+        console.log(user);
+
         return user
             ? {
                   id: user.id,
@@ -30,7 +32,7 @@ module.exports = class UserService {
 
     static async createUser(data = {}) {
         const createdUser = await this.#userRepository.create(data);
-        const roleUser = await this.#roleRepository.findOne({
+        const roleUser = await this.role.findOne({
             where: { name: 'user' },
         });
         createdUser.addRole(roleUser);
