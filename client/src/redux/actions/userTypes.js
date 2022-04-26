951bc62d161f01e4bf5d591f5244d10d47579a69
Umar Khalilov <ERMASTER100@gmail.com>
2022-04-26 22:06:04 +0300

    Add nessesary libs and action types

---
+++ client/src/redux/actions/userTypes.js
@@ -0,0 +1,19 @@
+export const ACTION_USER_TYPES = {
+    CREATE_USER_REQUEST: 'user/createRequest',
+    CREATE_USER_SUCCESS: 'user/createSuccess',
+    CREATE_USER_ERROR: 'user/createError',
+
+    GET_USER_REQUEST: 'user/getRequest',
+    GET_USER_SUCCESS: 'user/getSuccess',
+    GET_USER_ERROR: 'user/getError',
+
+    UPDATE_USER_REQUEST: 'user/updateRequest',
+    UPDATE_USER_SUCCESS: 'user/updateSuccess',
+    UPDATE_USER_ERROR: 'user/updateError',
+
+    DELETE_USER_REQUEST: 'user/deleteRequest',
+    DELETE_USER_SUCCESS: 'user/deleteSuccess',
+    DELETE_USER_ERROR: 'user/deleteError',
+
+    CLEAR_USER_ERROR: 'user/clearError',
+};
